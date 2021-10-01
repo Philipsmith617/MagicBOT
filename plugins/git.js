fcea798b682a14ddfb591fc11fb07c3daccc7cee
Philipsmith6175 <89512325+Philipsmith6175@users.noreply.github.com>
2021-10-01 11:25:53 +0100

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,21 +11,21 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/yCYYyEJ.jpeg";
+  r_text[0] = "https://www.linkpicture.com/q/Screenshot_20211001-083824_1.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number wa.me/919895828468*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creator Philip Smith*
+*owner number wa.me/2348159983206*
 
 *Bot making video : https://youtu.be/fd0KPblop-k*
 
 *Yt_PASSWORD: _________(password on this video watch it full)*
 
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
+*githublink : https://github.com/Philipsmith6175/Philsmagic*
 
 *audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
 `}) 
